a8e9408f10976e246697e7c0667c8dc822250b2c
Christian Ackermann <caackermann@uc.cl>
2018-10-24 10:23:52 -0300

    Revert "Routes"

--- app/models/reaction.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const reactionSchema = mongoose.Schema({
-      username        : String,
-      reaction_id     : Number,
-      message_id      : Number
-});
-
-module.exports = mongoose.model('Reaction', reactionSchema);
