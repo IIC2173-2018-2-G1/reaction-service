28606bde124eda0eadda61fbaedcbb61ccf09388
jiaranda <jiaranda@uc.cl>
2018-10-31 15:32:52 -0300

    indent fixed on routes.js file

--- app/routes.js
+++ app/routes.js
@@ -8,33 +8,33 @@ app.get('/', (req, res) => {
     })
   })
 
-  // get reactions from single message
-  app.get('/messages/:id/reactions', (req, res) => {
-      const id = parseInt(req.params.id, 10);
-      return res.status(404).send({
-        success: 'false',
-        message: 'message does not exist',
-      });
+// get reactions from single message
+app.get('/messages/:id/reactions', (req, res) => {
+    const id = parseInt(req.params.id, 10);
+    return res.status(404).send({
+      success: 'false',
+      message: 'message does not exist',
     });
+  });
 
-  //add reaction to message
-  app.post('/messages/:message_id/reactions', (req, res) => {
-      const messageId = req.params.message_id
-      const reactionId = req.query.reaction_id
-      const username = req.get('Username')
-      // create reaction
-      const reaction = new Reaction({
-        username: username,
-        message_id: messageId,
-        reaction_id: reactionId
-      })
-      reaction.save().then(() => console.log('new reaction created'))
+//add reaction to message
+app.post('/messages/:message_id/reactions', (req, res) => {
+    const messageId = req.params.message_id
+    const reactionId = req.query.reaction_id
+    const username = req.get('Username')
+    // create reaction
+    const reaction = new Reaction({
+      username: username,
+      message_id: messageId,
+      reaction_id: reactionId
+    })
+    reaction.save().then(() => console.log('new reaction created'))
 
-      // response
-      return res.status(201).send({
-        success: true,
-        message: 'reaction added successfully'
-      })
+    // response
+    return res.status(201).send({
+      success: true,
+      message: 'reaction added successfully'
+    })
     });
 
 }
