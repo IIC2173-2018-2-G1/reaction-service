e6905d8a5efdf0d053daa1bcde2af8f2859b05a0
jiaranda <jiaranda@uc.cl>
2018-11-02 21:30:08 -0300

    check params

--- app/routes.js
+++ app/routes.js
@@ -22,6 +22,20 @@ app.post('/messages/:message_id/reactions', (req, res) => {
     const messageId = req.params.message_id
     const reactionId = req.query.reaction_id
     const username = req.get('Username')
+    // check messageId, reactionId and username
+    if (!messageId) {
+      return res.status(401).send({
+        success: 'false',
+        message: 'message_id is required',
+      });
+    }
+
+    if (!reactionId) {
+      return res.status(401).send({
+        success: 'false',
+        message: 'reaction_id is required',
+      });
+    }
 
     Reaction.findOne({'message_id': messageId, 'username': username}, (err, reaction) => {
       // new reaction
@@ -31,19 +45,39 @@ app.post('/messages/:message_id/reactions', (req, res) => {
           message_id: messageId,
           reaction_id: reactionId
         })
-        newReaction.save().then(() => console.log('new reaction created'))
+        newReaction.save()
+        .then(() => {
+          // response
+          return res.status(201).send({
+            success: true,
+            message: 'reaction added successfully'
+          })
+        })
+        .catch(() => {
+          return res.status(500).send({
+            success: 'false',
+            message: 'internal error',
+          });
+        })
       }
       // update reaction
       else {
         reaction.reaction_id = reactionId
-        reaction.save().then(() => console.log('reaction changed'))
+        reaction.save()
+        .then(() => {
+          // response
+          return res.status(201).send({
+            success: true,
+            message: 'reaction added successfully'
+          })
+        })
+        .catch(() => {
+          return res.status(500).send({
+            success: 'false',
+            message: 'internal error',
+          });
+        })
       }
     })
-    // response
-    return res.status(201).send({
-      success: true,
-      message: 'reaction added successfully'
-    })
-    });
-
+  });
 }
